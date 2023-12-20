7befb3ed1a9120503bdc590a378aeace683af244
saarakhalid <khalidsaara004@gmail.com>
2023-12-20 12:31:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  let messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(messages);
+
+
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(5); 
+
+function countDownwhile(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
+  countDown(10); 
+  
